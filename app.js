967caacdec19a32332aff1ea630d681260d5eec8
Diego Araneda <daranedag@gmail.com>
2025-08-25 03:01:24 -0400

    CORS Update

--- app.js
+++ app.js
@@ -10,7 +10,8 @@ const app = express();
 
 const allowedOrigins = [
   'http://localhost:5173',
-  'https://observa2037.netlify.app'
+  'https://observalosrios.netlify.app',
+  'https://observalosrios.cl'
 ];
 app.use(cors({
   origin: function(origin, callback) {
